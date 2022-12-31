ae3d3037fe49cb2a78d98187786a87a7892d07d1
Ubuntu <asostrife@AsoStrifeVM.dsa54n2pjzhe3dpk305ja2ozfh.ax.internal.cloudapp.net>
2022-12-31 13:46:16 +0000

    Update server.js with https

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://azure.andreacorriga.com/my-movies/',
+  url: 'https://azure.andreacorriga.com/my-movies/',
   app: {
     keys: env.array('APP_KEYS'),
   },
